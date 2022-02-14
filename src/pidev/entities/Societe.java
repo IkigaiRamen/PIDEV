69fdd9e027a1a54607fd2b85f762fc3a47884be2
Aya Chlibi <ayachlibi@gmail.com>
2022-02-14 21:05:39 +0100

    gestion des utilisateurs

---
+++ src/pidev/entities/Societe.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pidev.entities;
+
+/**
+ *
+ * @author eya
+ */
+public class Societe {
+    private int id;
+    private String description;
+    private String adresse;
+    private String categorie;
+    private int idEmployeur;
+
+    public Societe() {
+    }
+
+    public Societe(String description, String adresse, String categorie, int idEmployeur) {
+        this.description = description;
+        this.adresse = adresse;
+        this.categorie = categorie;
+        this.idEmployeur = idEmployeur;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getCategorie() {
+        return categorie;
+    }
+
+    public void setCategorie(String categorie) {
+        this.categorie = categorie;
+    }
+
+    public int getIdEmployeur() {
+        return idEmployeur;
+    }
+
+    public void setIdEmployeur(int idEmployeur) {
+        this.idEmployeur = idEmployeur;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 79 * hash + this.id;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Societe other = (Societe) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Societe{" + "id=" + id + ", description=" + description + ", adresse=" + adresse + ", categorie=" + categorie + '}';
+    }
+    
+    
+}
