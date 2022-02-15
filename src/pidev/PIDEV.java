d9ea4505c3e743e6c6acdc41776ed505c4548486
dina <dina.benmansour@esprit.tn>
2022-02-15 09:37:53 +0100

    dina files , services commentaire/post

--- src/pidev/PIDEV.java
+++ src/pidev/PIDEV.java
@@ -5,6 +5,13 @@
  */
 package pidev;
 
+import java.util.ArrayList;
+import java.util.List;
+import pidev.entities.Commentaire;
+import pidev.entities.Post;
+import pidev.services.CommentaireService;
+import pidev.services.PostService;
+
 /**
  *
  * @author Khammessi
@@ -16,6 +23,31 @@ public class PIDEV {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    }
+ // con.getCnx();
+     
+     //-------------------------------- Commentaire -------------------------------------------------//
+        Commentaire C1 = new Commentaire(1,"dina","20/11/1999");
+        List<Commentaire> cs= new ArrayList<>();
+        CommentaireService CS= new CommentaireService();
+        CS.ajouterCommentaire(C1);
+        // cs=CS.afficherCommentaire();
+     // CS.updateCommentaire( 3,"aa", "bbb"); 
+      // CS.supprimerCommentaire(4); 
+       
+       
+       
+       //------------------------------ Post -------------------------------------------------//
+       
+       Post p1 = new Post(1,"aann","21/08/2000");
+        Post p2 = new Post(2,"hjjhj","10/02/2020");
+        PostService PS= new PostService();
+        //PS.ajouterPost(p1); 
+       // PS.ajouterPost(p2);
+      //List<Post> liste_poste= PS.afficherPost(); 
+      //PS.supprimerPost(2); 
+     //PS.updatePost(2, "ner","20/08/2200"); 
+     
+        
+          }
     
 }
