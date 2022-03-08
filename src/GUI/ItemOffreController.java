71df8acd25e39123968802963a7dea3656486acb
IkigaiRamen <55280193+IkigaiRamen@users.noreply.github.com>
2022-03-08 04:08:22 +0100

    Merge branch 'master' of https://github.com/IkigaiRamen/PIDEV

--- src/GUI/ItemOffreController.java
+++ src/GUI/ItemOffreController.java
@@ -7,7 +7,10 @@ package GUI;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 
 /**
  * FXML Controller class
@@ -16,12 +19,38 @@ import javafx.fxml.Initializable;
  */
 public class ItemOffreController implements Initializable {
 
+    @FXML
+    private Label Titre;
+    @FXML
+    private Label Poste;
+    @FXML
+    private Label Salaire;
+    @FXML
+    private Label datelimite;
+    @FXML
+    private ImageView img1 ;
+    @FXML
+    private ImageView img2 ;
+    
     /**
      * Initializes the controller class.
      */
+    
+    
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        
+        
+        
+        
     }    
+    
+    
+    
+    
+    
+    
     
 }
